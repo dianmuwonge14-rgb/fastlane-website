b1e0412140a1126c368ab7e20f91897a8415562d
dianmuwonge14-rgb <dianmuwonge14@gmail.com>
2026-04-01 12:02:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // ===== SCROLL ANIMATION =====
     const animatedItems = document.querySelectorAll(
-        ".why-card, .testimonial-card, .faq-item, .hero h1"
+        ".why-card, .testimonial-card, .faq-item, .hero h1, .brand-card"
     );
 
     // Add initial class
