1b9c0da608d91083a4d95ed9af3cab10ffe3e54e
dianmuwonge14-rgb <dianmuwonge14@gmail.com>
2026-04-02 04:56:24 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,20 @@
-// IMPORT FIREBASE
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 // YOUR CONFIG
 const firebaseConfig = {
-  apiKey: "AIzaSyA2MBWKrUuZWS3XRQVv3836CVwQS7NrG_4",
-  authDomain: "fastlane-marketplace.firebaseapp.com",
-  projectId: "fastlane-marketplace",
-  storageBucket: "fastlane-marketplace.firebasestorage.app",
-  messagingSenderId: "193589920178",
-  appId: "1:193589920178:web:dd0aec5dd6b1a8a7a3e4ab",
-  measurementId: "G-04F57XHYP3"
+    apiKey: "AIzaSyA2MBWKrUuZWS3XRQVv3836CVwQS7NrG_4",
+    authDomain: "fastlane-marketplace.firebaseapp.com",
+    projectId: "fastlane-marketplace",
+    storageBucket: "fastlane-marketplace.firebasestorage.app",
+    messagingSenderId: "193589920178",
+    appId: "1:193589920178:web:dd0aec5dd6b1a8a7a3e4ab",
+    measurementId: "G-04F57XHYP3"
 };
 
-// INITIALIZE FIREBASE
+// INIT
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-// EXPORT AUTH
-export const auth = getAuth(app);
+// MAKE GLOBAL
+window.db = db;
