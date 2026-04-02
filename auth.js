9f64ea712ffa0ecd2bf22598a760cf343628bea1
dianmuwonge14-rgb <dianmuwonge14@gmail.com>
2026-04-02 03:01:06 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,25 @@
+import { auth } from "./firebase.js";
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+// SIGN UP
+document.getElementById("signupBtn").onclick = () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then(() => alert("Account created successfully"))
+        .catch(err => alert(err.message));
+};
+
+// LOGIN
+document.getElementById("loginBtn").onclick = () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then(() => alert("Login successful"))
+        .catch(err => alert(err.message));
+};
